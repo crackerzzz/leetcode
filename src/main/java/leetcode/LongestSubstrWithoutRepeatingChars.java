86976c88902eb03757e4ec165f7c2578b56eda99
crackerzzz <shreejwal.shrestha@gmail.com>
2018-01-10 13:44:27 +1100

    added multiple valid functions

--- src/main/java/leetcode/LongestSubstrWithoutRepeatingChars.java
+++ src/main/java/leetcode/LongestSubstrWithoutRepeatingChars.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -53,6 +54,33 @@ public class LongestSubstrWithoutRepeatingChars {
 	}
 
 	static class InnerClass {
+		public int lengthOfLongestSubstring1(String s) {
+			int current_len = 0, max_length = 0;
+			String current_nrcs = "";
+			Set<Character> visited = new HashSet<>();
+			for (int i = 0; i < s.toCharArray().length; i++) {
+				final char c = s.charAt(i);
+				if (!visited.contains(c)) {
+					visited.add(c);
+					current_len++;
+					max_length = Math.max(max_length, current_len);
+					current_nrcs = current_nrcs.concat(c + "");
+				}
+				else {
+					int index = current_nrcs.indexOf(c);
+					current_nrcs = current_nrcs
+						.substring(index + 1, current_nrcs.length())
+						.concat(c + "");
+					current_len = current_nrcs.length();
+					// visited.clear();
+					// for (char x : current_nrcs.toCharArray()) {
+					// visited.remove(x);
+					// }
+				}
+			}
+			return max_length;
+		}
+
 		public int lengthOfLongestSubstring(String s) {
 			int current_len = 0, max_length = 0;
 			String current_nrcs = "";
@@ -71,13 +99,58 @@ public class LongestSubstrWithoutRepeatingChars {
 						.substring(index + 1, current_nrcs.length())
 						.concat(c + "");
 					current_len = current_nrcs.length();
-//					visited.clear();
-//					for (char x : current_nrcs.toCharArray()) {
-//						visited.remove(x);
-//					}
+					visited.clear();
 				}
 			}
 			return max_length;
+		}
+
+		/***
+		 * the basic idea is, keep a hashmap which stores the characters in string as keys
+		 * and their positions as values, and keep two pointers which define the max
+		 * substring. move the right pointer to scan through the string , and meanwhile
+		 * update the hashmap. If the character is already in the hashmap, then move the
+		 * left pointer to the right of the same character last found. Note that the two
+		 * pointers can only move forward.
+		 * @param s
+		 * @return
+		 */
+		public int lengthOfLongestSubstring2(String s) {
+			if (s.length() == 0)
+				return 0;
+			HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+			int max = 0;
+			for (int i = 0, j = 0; i < s.length(); ++i) {
+				if (map.containsKey(s.charAt(i))) {
+					j = Math.max(j, map.get(s.charAt(i)) + 1);
+				}
+				map.put(s.charAt(i), i);
+				max = Math.max(max, i - j + 1);
+			}
+			return max;
+		}
+
+		// The idea is use a hash set to track the longest substring without repeating
+		// characters so far, use a fast pointer j to see if character j is in the hash
+		// set or not, if not, great, add it to the hash set, move j forward and update
+		// the max length, otherwise, delete from the head by using a slow pointer i until
+		// we can put character j to the hash set.
+
+		public int lengthOfLongestSubstring4(String s) {
+			int i = 0, j = 0, max = 0;
+			Set<Character> set = new HashSet<>();
+
+			while (j < s.length()) {
+				if (!set.contains(s.charAt(j))) {
+					set.add(s.charAt(j++));
+					max = Math.max(max, set.size());
+				}
+				else {
+					set.remove(s.charAt(i++));
+				}
+			}
+
+			return max;
 		}
 	}
 
